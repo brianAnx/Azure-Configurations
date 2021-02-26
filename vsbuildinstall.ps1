8a15966690521db9ebf5c06b69937e3a033aded8
Brian Russo <brusso@anexinet.com>
2021-02-26 15:27:23 -0500

    PowerShell install file

---
+++ vsbuildinstall.ps1
@@ -0,0 +1,5 @@
+﻿#install Chhocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+#Install VS for Build
+choco install visualstudio2019buildtools -y
