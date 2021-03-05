fac1a2fca28a6a39b727dc9ce4c458eb8092a71c
Brian Russo <brusso@anexinet.com>
2021-03-05 10:50:09 -0500

    updated install package

--- vsbuildinstall.ps1
+++ vsbuildinstall.ps1
@@ -1,5 +1,8 @@
 ﻿#install Chhocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-#Install VS for Build
-choco install visualstudio2019buildtools -y
+#Install VS Web Build Tools
+#choco install visualstudio2019buildtools -y
+#choco install visualstudio2019-workload-webbuildtools -y
+choco install visualstudio2019-workload-netweb -y
+#choco install visualstudio2017-workload-netweb -y
